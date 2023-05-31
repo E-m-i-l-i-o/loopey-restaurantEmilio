97891b85ea284e05d1a3c95b92512cbe3c7ba17f
Emilio Moreno <emilio@Emilios-MacBook-Air.local>
2023-05-31 14:04:08 +0200

    layout, views, and more

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
-
+const hbs = require("hbs"); //we require and store teh packagehbs
 const app = express();
 
 app.use(express.static('public')); // Make everything inside of public-> available
 
+app.set("views", __dirname + "/views"); ///tells our Express app where to look for our views
+app.set("view engine", "hbs");//sets HBS as the template engine
+
+hbs.registerPartials(__dirname + "/views/partials");//Im telling HBS where I want to store my partials
 
 //now we can apply methods to app
 // app.get(path, code); whenever you get a request to a specific path, execute this code (can be a function in any form.)
@@ -13,7 +17,9 @@ app.use(express.static('public')); // Make everything inside of public-> availab
 app.get("/", function(request, response, next){
     console.log("we have received a request for the HOMEPAGE");
     //response.send(``); //this can take html, images, and css as a string STATIC FILES. remember to give the image an absolute path
-    response.sendFile(__dirname + "/views/home-page.html");
+    //response.sendFile(__dirname + "/views/home-page.html");
+    response.render('home-page');
+
 
 });
 
@@ -21,9 +27,93 @@ app.get("/", function(request, response, next){
 // GET /contact (this is waht you type in the browser)
 app.get("/contact", (req, res, next) => {
     //res.send(" ");
-    res.sendFile(__dirname + "/views/contact-page.html");
+    //res.sendFile(__dirname + "/views/contact-page.html");
+    res.render("contact-page");
+
 
 
 });
 
-app.listen(3000, () => { console.log("server listening on port 3000...")});
+// app.get("/margarita", (req, res, next) => {
+//     //res.send(" ");
+//     console.log('marga')
+//     res.sendFile(__dirname + "/views/margarita.html");
+// });
+
+// app.get("/veggie", (req, res, next) => {
+//     //res.send(" ");
+//     console.log('veggie')
+//     res.sendFile(__dirname + "/views/veggie.html");
+// });
+
+// app.get("/seafood", (req, res, next) => {
+//     //res.send(" ");
+//     console.log('seafood')
+//     res.sendFile(__dirname + "/views/seafood.html");
+// });
+
+
+////////////////
+// instead of .sendFile() or .send() we now reuse code by rendering with HBS
+////////////////
+
+
+// GET /pizzas/margarita
+app.get("/pizzas/margarita", (req, res, send) => {
+    //res.send("page for margarita");
+    
+    const dataMargarita = {
+        title: "Pizza Margarita",
+        price: 15,
+        recommendedDrink: 'Beer',
+        imageFile: 'pizza-margarita.jpg',
+        ingredients: ['cherry tomatoes', 'basilicum', 'Olives']
+    }
+    
+    res.render('product', dataMargarita); //name of the rendered 'view' (page without extension & without slash at the beggining)
+});
+
+
+// GET /pizzas/veggie
+app.get("/pizzas/veggie", (req, res, send) => {
+    //res.send("page for veggie");
+
+    const dataVeggie = {
+        title: 'Veggie Pizza',
+        price: 13,
+        recommendedDrink: 'power smoothie',
+        imageFile: 'pizza-veggie.jpg',
+        ingredients: ['cherry tomatoes', 'basilicum', 'Olives'],
+      };
+    res.render('product', dataVeggie)
+
+
+
+});
+
+
+// GET /pizzas/seafood
+app.get("/pizzas/seafood", (req, res, next) => {
+   // res.send("page for seafood");
+
+   const dataSeafood = {
+    title: 'Seafood Pizza',
+
+    recommendedDrink: 'white wine',
+    imageFile: 'pizza-seafood.jpg',
+    ingredients: ['tomato sauce', 'garlic', 'prawn'],
+  };
+  res.render('product', dataSeafood);
+});
+
+
+
+app.listen(3400, () => { console.log("server listening on port 3400...")});
+
+//
+//
+//
+
+//npm start 
+//"dev": "kill -9 $(lsof -t -i:3400) && node app.js",
+// "start": "nodemon --exec npm run dev"
